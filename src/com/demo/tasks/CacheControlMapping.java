b147ffd8b4057fc8a0b9d00eb0fb06b096f49222
Aasif Khan <aasifk106@gmail.com>
2019-12-21 09:52:33 +0530

    commented all AWS tasks

--- src/com/demo/tasks/CacheControlMapping.java
+++ src/com/demo/tasks/CacheControlMapping.java
@@ -1,41 +1,41 @@
-package com.demo.tasks;
-
-public class CacheControlMapping
-{
-
-    private String extension;
-    private String cacheControl;
-
-    public CacheControlMapping()
-    {
-    }
-
-    public CacheControlMapping(String extension, String cacheControl)
-    {
-        setExtension(extension);
-        setCacheControl(cacheControl);
-    }
-
-    public void setExtension(String extension)
-    {
-        this.extension = extension;
-    }
-
-    public String getExtension()
-    {
-        return this.extension;
-    }
-
-    /**
-     * @param cacheControl The cache-control string eg. max-age=3600
-     */
-    public void setCacheControl(String cacheControl)
-    {
-        this.cacheControl = cacheControl;
-    }
-
-    public String getMaxAge()
-    {
-        return this.cacheControl;
-    }
-}
+//package com.demo.tasks;
+//
+//public class CacheControlMapping
+//{
+//
+//    private String extension;
+//    private String cacheControl;
+//
+//    public CacheControlMapping()
+//    {
+//    }
+//
+//    public CacheControlMapping(String extension, String cacheControl)
+//    {
+//        setExtension(extension);
+//        setCacheControl(cacheControl);
+//    }
+//
+//    public void setExtension(String extension)
+//    {
+//        this.extension = extension;
+//    }
+//
+//    public String getExtension()
+//    {
+//        return this.extension;
+//    }
+//
+//    /**
+//     * @param cacheControl The cache-control string eg. max-age=3600
+//     */
+//    public void setCacheControl(String cacheControl)
+//    {
+//        this.cacheControl = cacheControl;
+//    }
+//
+//    public String getMaxAge()
+//    {
+//        return this.cacheControl;
+//    }
+//}
